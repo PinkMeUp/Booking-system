6e66ab39fd71431eab99c97e0af9f5e9ed93482b
Madala331 <2799331@students.wits.ac.za>
2026-04-23 21:03:07 +0200

    Add files via upload

---
+++ prototype/script.js
@@ -0,0 +1,47 @@
+
+const admins = [
+    { email: "admin@salon.com", password: "123456" },
+    { email: "manager@salon.com", password: "abcdef" }
+];
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const email = document.querySelector('input[type="email"]').value.trim();
+    const password = document.querySelector('input[type="password"]').value.trim();
+
+   
+    if (email === "" || password === "") {
+        alert("Please fill in all fields");
+        return;
+    }
+
+    
+    if (!email.includes("@") || 
+       (!email.endsWith(".com") && !email.endsWith(".co.za"))) {
+        alert("Enter a valid email");
+        return;
+    }
+
+   
+    const user = admins.find(admin => 
+        admin.email.toLowerCase() === email.toLowerCase() &&
+        admin.password === password
+    );
+
+    
+    if (user) {
+        
+        window.location.href = "dashboard.html";
+    } else {
+        alert("Invalid email or password");
+    }
+});
+
+const logoutBtn = document.getElementById("logoutBtn");
+
+logoutBtn.addEventListener("click", function() {
+    window.location.href = "login.html";
+});
